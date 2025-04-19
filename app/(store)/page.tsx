c4b10ba936aaf609302e126a46cd9ebdd8ef30e8
Breno Oliveira <brenooliveiraa@gmail.com>
2025-04-19 20:22:12 -0300

    chore: initial project setup with Next.js

---
+++ app/(store)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+<div>
+  HELLO WORLD!
+<Button>CLICK ME</Button>
+</div>
+  );
+}
