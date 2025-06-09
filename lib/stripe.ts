f117da75a62eadfc7bc1045a0817547617aeedd0
Breno Oliveira <brenooliveiraa@gmail.com>
2025-06-09 20:57:41 -0300

    chore: initial Stripe integration setup

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,13 +1,13 @@
-import Stripe from 'stripe'
+// import Stripe from 'stripe'
 
-if(!process.env.STRIPE_API_KEY){
-    throw new Error("STRIPE_API_KEY not set")
-}
+// if(!process.env.STRIPE_API_KEY){
+//     throw new Error("STRIPE_API_KEY not set")
+// }
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-    apiVersion: '2025-05-28.basil',
-})
+// const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+//     apiVersion: '2025-05-28.basil',
+// })
 
 
 
-export default stripe
+// export default stripe
