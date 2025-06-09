f78a7743f0615b1e29c3c3bc9d03df839ef8fa25
Breno Oliveira <brenooliveiraa@gmail.com>
2025-06-09 16:30:31 -0300

    feat: add and configure BasketPage with grouped items and product details

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,12 +5,15 @@ import Link from 'next/link'
 import React from 'react'
 import Form from 'next/form'
 import { PackageIcon, TrolleyIcon } from '@sanity/icons'
+import { useBasketStore } from '@/app/(store)/store'
 
 
 
 
 export default function Header() {
   const { user } = useUser()
+  const itemCount = useBasketStore((state)=> state.items.reduce((total, item)=> total + item.quantity, 0 )
+  )
 
 
 
@@ -55,10 +58,16 @@ export default function Header() {
 
         <div className='flex items-center space-x-4 mt-4 sm:mt-0 flx-1 sm:flex-none'>
           <Link href='/basket'
-            className='flex-1 relative flex justify-center sm:justify-start sm:flex-none items-center space-x-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
+            className='relative flex justify-center sm:justify-start sm:flex-none items-center space-x-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
           >
             <TrolleyIcon className='w-6 h-6' />
-            {/* fazer um contador de itens dps com global state*/}
+          
+            <span
+              className='
+              absolute -top-2 -right-2 bg-red-500 text-white rounded-full w-5 h-5 items-center justify-center text-center text-xs'
+            >
+              {itemCount}
+            </span>
             <span> My Basket</span>
           </Link>
           {/* area do usuario*/}
